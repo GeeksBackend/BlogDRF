e3994845ef76a2d25aac5e8ebb07a06f55ca88cb
Toktorov <ktoktorov2005@gmail.com>
2023-03-13 20:53:32 +0600

    postgresql ready

--- blog_rest/settings.py
+++ blog_rest/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'blog_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'simpledjango',
+    'USER': 'simpledjangouser',
+    'PASSWORD': '123simpledjango123',
+    'HOST': 'localhost',
+    'PORT': '',
     }
 }
 
